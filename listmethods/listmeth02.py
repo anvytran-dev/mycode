615e7f2a68a829f02baa87c6077e5845c29b61a3
Anvy Tran <anvytran-dev@users.noreply.github.com>
2022-01-04 22:32:57 +0000

    learning about list methods

---
+++ listmethods/listmeth02.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+proto = ["ssh", "http", "https"]
+protoa = ["ssh", "http", "https"]
+print(proto)
+proto.append("dns") # this line will add "dns" to the end of our list
+protoa.append("dns") # this line will add "dns" to the end of our list
+print(proto)
+proto2 = [ 22, 80, 443, 53 ] # a list of common ports
+proto.extend(proto2) # pass proto2 as an argument to the extend method
+print(proto)
+protoa.append(proto2) # pass proto2 as an argument to the append method
+print(protoa)
+
+# insert method
+proto.insert(1, "hehe")
+print(proto)
+
+# reverse  method
+proto2.reverse()
+print(proto2)
+
+# sort method
+proto2.sort()
+print(proto2)
